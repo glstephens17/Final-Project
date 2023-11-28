b99af6f8a43ecae325252fc9c6e40503f7c18688
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-27 22:19:13 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -2,7 +2,7 @@
 <footer style=" position: absolute;
   bottom: 0;
   width: 100%;
-  height: 2.5rem;">
+  height: 2.5rem;text-align:center;">
  &copy; All Rights Reserved to GabSpotify 2023
 </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
