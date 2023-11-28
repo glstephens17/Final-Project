6b3e6aab595ce2002aec27a3b4f3fa24ca04f55a
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-27 22:41:05 -0600

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -3,8 +3,7 @@ require_once("util-db.php");
 require_once("model-playlist.php");
 
 $pageTitle = "Playlists";
-include "view-header.php";
-$kids = selectPlaylists();
+
+$playlists = selectPlaylists();
 include "view-playlist.php";
-include "view-footer.php";
 ?>
