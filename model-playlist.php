1263f1ef6e7d6769cc3f84219f534afcadeb273e
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-27 22:34:13 -0600

    Create model-playlist.php

---
+++ model-playlist.php
@@ -0,0 +1,15 @@
+<?php
+function selectPlaylists() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Playlist_Id,Playlist_Title,No_of_Songs FROM `Playlist`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
