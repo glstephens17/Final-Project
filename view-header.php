b9d0356e11609ea104721a0be2d897a02b044012
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-02 22:17:31 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -29,7 +29,7 @@
           <a class="nav-link" href="artists.php">Top 10 Artists</a>
         </li> 
          <li class="nav-item">
-          <a class="nav-link" href="parents-with-kids.php">Playlist You May Like</a>
+          <a class="nav-link" href="playlists.php">Playlists You May Like</a>
         </li> 
       </ul>
     </div>
