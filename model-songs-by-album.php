9dd952b917d26f463e4e2badb01dfe4948bea612
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-02 22:37:32 -0600

    Update model-songs-by-album.php

--- model-songs-by-album.php
+++ model-songs-by-album.php
@@ -1,1 +1,16 @@
-
+<?php
+function selectSongsByAlbum($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("");
+        $stmt->bind_param("i", $pid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
