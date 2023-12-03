397dbf1eb6ae2aa79ee04eb681a5e02a940c5783
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-02 21:50:52 -0600

    Update view-albums.php

--- view-albums.php
+++ view-albums.php
@@ -14,10 +14,9 @@
  ?>
    <tr>
      <td><?php echo $album['Album_Title']; ?></td>
-     <td><?php echo $album['Album_Id']; ?></td>
-     <td><?php echo $album['Artist_Name']; ?></td>
+       <td><?php echo $album['Artist_Name']; ?></td>
      <td><?php echo $album['No_of_Songs']; ?></td>
-     <td><a href="parents-pets.php?id=<?php echo $parent['parent_id']; ?>"</a><td>
+     <td><a href="albums.php?id=<?php echo $album['album_id']; ?>"</a><td>
    </tr>
  <?php  
  }
