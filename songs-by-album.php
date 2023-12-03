6e06b8a200e1fd7217cba09c9710aaa09f0893dc
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-02 22:36:33 -0600

    Update songs-by-album.php

--- songs-by-album.php
+++ songs-by-album.php
@@ -1,1 +1,10 @@
 
+<?php
+require_once("util-db.php");
+require_once("model-songs-by-album.php");
+$pageTitle = "Songs From Album";
+include "view-header.php";
+$songs = selectSongsByAlbum($_POST['pid']);
+include "view-songs-by-album.php";
+include "view-footer.php";
+?>
