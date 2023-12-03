73f4e85728242acc8221b598fea5eed9ee1353da
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-02 21:45:46 -0600

    Update view-albums.php

--- view-albums.php
+++ view-albums.php
@@ -1,1 +1,27 @@
-
+<h1>Trending Albums</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Album Title</th>
+       <th>Artist</th>
+        <th>Number of Songs</th>
+      </tr>
+    </thead>
+    <tbody>
+ <?php
+ while ($album = $albums->fetch_assoc()) {
+ ?>
+   <tr>
+     <td><?php echo $album['Album_Title']; ?></td>
+     <td><?php echo $album['Album_Id']; ?></td>
+     <td><?php echo $album['Artist_Name']; ?></td>
+     <td><?php echo $album['No_of_Songs']; ?></td>
+     <td><a href="parents-pets.php?id=<?php echo $parent['parent_id']; ?>"</a><td>
+   </tr>
+ <?php  
+ }
+ ?>      
+    </tbody>
+  </table>
+</div>
