ff341916ffeca97f62735567683779b05ab88b5f
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-02 22:09:33 -0600

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -1,1 +1,28 @@
-
+<h1>Artists</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Artist Name</th>
+      <th>Years Active</th>
+      <th>Stage Name</th>
+        <th>Genre</th>
+      </tr>
+    </thead>
+    <tbody>
+ <?php
+ while ($artist = $artists->fetch_assoc()) {
+ ?>
+   <tr>
+     <td><?php echo $artist['Artist_Name']; ?></td>
+     <td><?php echo $artist['Years_Active']; ?></td>
+     <td><?php echo $artist['Stage_Name']; ?></td>
+     <td><?php echo $artist['Genre']; ?></td>
+     <td><a href="parents-pets.php?id=<?php echo $parent['parent_id']; ?>"</a><td>
+   </tr>
+ <?php  
+ }
+ ?>      
+    </tbody>
+  </table>
+</div>
