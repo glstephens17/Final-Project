5e1806b4111f29ff2818375083d9e6ba44ddd9d4
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-02 21:41:33 -0600

    Update model-albums.php

--- model-albums.php
+++ model-albums.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectSongs() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Album_Title, a.Album_Id, a.Artist_Name, No_of_Songs FROM `Song` s JOIN Artist a ON s.Artist_Id = a.Artist_Id JOIN Album al ON s.Album_Id = al.Album_Id LIMIT 10;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
