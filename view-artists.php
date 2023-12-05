93faf858f198788dc6371d73e6383495b7d81eb7
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-04 21:56:12 -0600

    Update view-artists.php

--- view-artists.php
+++ view-artists.php
@@ -27,7 +27,7 @@ include "view-artists-newform.php";
     ?></td>
    <td>
   <form method="post" action="">
-    <input type="hidden" name="aid" value="<?php echo $artist['artist_id']; ?>">
+    <input type="hidden" name="aid" value="<?php echo $artist['Artist_Id']; ?>">
     <input type="hidden" name="actionType" value="Delete">
     <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure you want to Delete?')">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
