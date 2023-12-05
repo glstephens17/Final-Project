e019195e61cb4272ab0ed3752bbdb85de1bc66df
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-04 22:22:53 -0600

    Update model-songs.php

--- model-songs.php
+++ model-songs.php
@@ -2,7 +2,7 @@
 function selectSongs() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Song_Title, s.Song_Id, a.Artist_Name, a.Stage_Name al.Album_Title, Duration FROM `Song` s JOIN Artist a ON s.Artist_Id = a.Artist_Id JOIN Album al ON s.Album_Id = al.Album_Id LIMIT 100;");
+        $stmt = $conn->prepare("SELECT Song_Title, s.Song_Id, a.Artist_Name, a.Stage_Name, al.Album_Title, Duration FROM `Song` s JOIN Artist a ON s.Artist_Id = a.Artist_Id JOIN Album al ON s.Album_Id = al.Album_Id LIMIT 10;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
