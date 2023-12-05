a8f3725abdeaab10f9a82582692a1051727fcc17
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-05 15:53:59 -0600

    Update albums.php

--- albums.php
+++ albums.php
@@ -2,8 +2,39 @@
 require_once("util-db.php");
 require_once("model-albums.php");
 
-$pageTitle = "Trending Albums";
+$pageTitle = "Albums";
 include "view-header.php";
+if(isset($_POST['actionType'])){
+  switch($_POST['actionType']){
+    case "Add":
+        if(insertAlbums($_POST['alid'],$_POST['altitle'],$_POST['aid'], $_POST['noofsongs'] )){          
+          echo '<div class="alert alert-success" role="alert">Added a new artist successfully!</div>';
+        }
+        else{
+          echo '<div class="alert alert-danger" role="alert">There was an error adding the Album</div>';
+        }
+        break;
+    case "Edit":
+           if(updateAlbums($_POST['alid'],$_POST['altitle'],$_POST['aid'], $_POST['noofsongs']))
+               {
+          echo '<div class="alert alert-success" role="alert"> Album successfully edited!</div>';
+          }
+          else
+          {
+          echo '<div class="alert alert-danger" role="alert"> Error</div>';
+          }
+          break;
+    case "Delete":
+        if (deleteAlbums($_POST['alid']))
+        {
+        echo '<div class="alert alert-success" role="alert"> Album successfully deleted!</div>';
+        }
+        else {
+        echo '<div class="alert alert-danger" role="alert"> Error</div>';
+        }
+  }
+}
+
 $albums = selectAlbums();
 include "view-albums.php";
 include "view-footer.php";
