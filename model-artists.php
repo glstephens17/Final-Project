b9da8314e497518f6214d586d57c5cf8c343e714
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-04 21:52:05 -0600

    Update model-artists.php

--- model-artists.php
+++ model-artists.php
@@ -2,7 +2,7 @@
 function selectArtists() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Artist_Name, Artist_Id, Years_Active, Stage_Name, Genre FROM `Artist` WHERE 1");
+        $stmt = $conn->prepare("SELECT Artist_Name, Artist_Id, Years_Active, Stage_Name, Genre FROM `Artist`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
