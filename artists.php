e9aa1ed985f398efbf253fcbcf21dc93b4fda4e2
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-04 21:23:42 -0600

    Update artists.php

--- artists.php
+++ artists.php
@@ -4,6 +4,36 @@ require_once("model-artists.php");
 
 $pageTitle = "Artists";
 include "view-header.php";
+  if(isset($_POST['actionType'])){
+  switch($_POST['actionType']){
+    case "Add":
+        if(insertArtist($_POST['aName'],$_POST['yearActive'],$_POST['stageName'], $_POST['genre'] )){
+          echo '<div class="alert alert-success" role="alert">Added a new artist successfully!</div>';
+        }
+        else{
+          echo '<div class="alert alert-danger" role="alert">There was an error adding the artist</div>';
+        }
+        break;
+    case "Edit":
+          if(updateKid($_POST['kidName'], $_POST['kidBirthYear'], $_POST['pid'], $_POST['kId']))
+               {
+          echo '<div class="alert alert-success" role="alert"> Kid successfully edited!</div>';
+          }
+          else
+          {
+          echo '<div class="alert alert-danger" role="alert"> Error</div>';
+          }
+          break;
+    case "Delete":
+        if (deleteKid($_POST['kId']))
+        {
+        echo '<div class="alert alert-success" role="alert"> Kid successfully deleted!</div>';
+        }
+        else {
+        echo '<div class="alert alert-danger" role="alert"> Error</div>';
+        }
+  }
+}
 $artists = selectArtists();
 include "view-artists.php";
 include "view-footer.php";
