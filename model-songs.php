7e7a14b21b9f9fb83a1db08c210df3519aaa67f0
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-04 22:15:51 -0600

    Update model-songs.php

--- model-songs.php
+++ model-songs.php
@@ -12,4 +12,43 @@ function selectSongs() {
         throw $e;
     }
 }
+function insertSong($aName, $yearActive, $stageName, $genre) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Song` (`Artist_Name`, `Years_Active`, `Stage_Name`, `Genre`) VALUES (?, ?, ?, ?);");
+        $stmt->bind_param("siss", $aName, $yearActive, $stageName, $genre);
+        $success = $stmt->execute();
+            $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updatSong($aName, $yearActive, $stageName, $genre, $aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Song` set `Artist_Name` = ?, `Years_Active` = ?, `Stage_Name` = ?, `Genre` = ? WHERE `artist_id` = ?");
+        $stmt->bind_param("sissi",$aName, $yearActive, $stageName, $genre, $aid);
+        $success = $stmt->execute();
+            $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteSong($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `Song` WHERE `Song_Id` = ?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+            $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
