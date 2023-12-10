1f0e6c5fdb524507028ef677c66611976fea35f4
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 12:18:35 -0600

    Create view-album-input-list.php

---
+++ view-album-input-list.php
@@ -0,0 +1,12 @@
+<select class="form-select" id="alId" name="alId"> 
+<?php while ($albumItem = $albumList->fetch_assoc()) {
+$selText="";
+if($selectedalbum==$albumItem['Album_Id'])
+{
+$selText=" selected";
+}
+?>
+<option value="<?php echo $albumItem['Album_Id'];?>"<?=$selText?>><?php echo
+$albumItem['Album_Title']; ?></option>
+<?php } ?>
+</select>
