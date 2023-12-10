fd5fcaf75946dfc78cd58cb6ff4d949af094182b
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 12:15:04 -0600

    Update model-songs.php

--- model-songs.php
+++ model-songs.php
@@ -51,4 +51,17 @@ function deleteSong($sid) {
         throw $e;
     }
 }
+function selectArtistsForInput() {
+try {
+$conn = get_db_connection();
+$stmt = $conn->prepare("SELECT Artist_id, stage_name FROM `Artist` order by stage_name;");
+$stmt->execute();
+$result = $stmt->get_result();
+$conn->close();
+return $result;
+} catch (Exception $e) {
+$conn->close();
+throw $e;
+}
+}
 ?>
