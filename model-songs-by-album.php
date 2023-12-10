3190a5596de313296e83f032cf3ce87ba5d18f91
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 13:55:11 -0600

    Update model-songs-by-album.php

--- model-songs-by-album.php
+++ model-songs-by-album.php
@@ -13,11 +13,11 @@ function selectSongsByPlaylist($pid) {
         throw $e;
     }
 }
-function selectSongsByAlbum($pid) {
+function selectSongsByAlbum($alId) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT Song_Title, s.Song_Id, a.Artist_Name, al.Album_Title, Duration FROM `Song` s JOIN Artist a ON s.Artist_Id = a.Artist_Id JOIN Album al ON s.Album_Id = al.Album_Id where al.Album_Id=?;");
-        $stmt->bind_param("i", $pid);
+        $stmt->bind_param("i", $alId);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
