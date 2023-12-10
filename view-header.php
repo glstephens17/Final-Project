2e8ff0509ec67319ce2422716980b95fdd1c4190
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 14:13:53 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -17,14 +17,14 @@
       <div class="container">
 <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/index.php">>Gab's Spotify</a>
+    <a class="navbar-brand" href="/index.php">Gab's Spotify</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarColor02">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link active" href="#">Gab's Playlists
+          <a class="nav-link active" href="playlist.php">Gab's Playlists
             <span class="visually-hidden">(current)</span>
           </a>
         </li>
