fde990e6c30fc282b85abfda219078e10797887f
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 12:55:24 -0600

    Update view-artist-input-list.php

--- view-artist-input-list.php
+++ view-artist-input-list.php
@@ -1,12 +1,12 @@
 <select class="form-select" id="aId" name="aId"> 
 <?php while ($artistItem = $artistList->fetch_assoc()) {
 $selText="";
-if($selectedArtist==$artistItem['artist_id'])
+if($selectedArtist==$artistItem['Artist_Id'])
 {
 $selText=" selected";
 }
 ?>
-<option value="<?php echo $artistItem['Artist_id'];?>"<?=$selText?>><?php echo
+<option value="<?php echo $artistItem['Artist_Id'];?>"<?=$selText?>><?php echo
 $artistItem['stage_name']; ?></option>
 <?php } ?>
 </select>
