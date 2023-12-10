58f2d02684e009ec6e36626b61d29fc0b590d126
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 11:58:24 -0600

    Create view-artist-input-list.php

---
+++ view-artist-input-list.php
@@ -0,0 +1,12 @@
+<select class="form-select" id="aId" name="aId"> 
+<?php while ($artistItem = $artistList->fetch_assoc()) {
+$selText="";
+if($selectedArtist==$artistItem['artist_id'])
+{
+$selText=" selected";
+}
+?>
+<option value="<?php echo $artistItem['artist_id'];?>"<?=$selText?>><?php echo
+$artistItem['stage_name']; ?></option>
+<?php } ?>
+</select>
