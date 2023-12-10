c04b27d1261cbda35bbf5c9fc352566516fc5f62
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 15:30:08 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -17,7 +17,7 @@
       <div class="container">
 <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/index.php">Gab's Spotify</a>
+    <a class="navbar-brand" href="/index.php">Gab's Playlists</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
