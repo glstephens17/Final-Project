6277c98855b9819d5a7f26661af487945b45e08a
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 13:53:33 -0600

    Update view-playlist.php

--- view-playlist.php
+++ view-playlist.php
@@ -13,7 +13,7 @@
         <div class="card-body">
           <h5 class="card-title"><?php echo $playlist['Playlist_Title']; ?></h5>
           <p class="card-text">There are <?php echo $playlist['No_of_Songs']; ?> songs in this playlist. This is one of the best playlists created by ...</p>
-          <form method="post" action="songs-by-album.php">
+          <form method="post" action="songs-by-playlist.php">
 
 <input type="hidden" name="pid" value="<?php echo $playlist['Playlist_Id']; ?>"/>
             <button type="submit" class="btn btn-primary">Songs</button>
