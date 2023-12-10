5ead500a3a159bfc38a13c17caeb4e93d2fbd609
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 13:12:22 -0600

    Update view-playlist.php

--- view-playlist.php
+++ view-playlist.php
@@ -1,27 +1,28 @@
-
-
 <div class="row">
- <?php
- while ($playlist = $playlists->fetch_assoc()) {
- ?>
-    <div class="col-sm-6">
-    <div class="card" style="width: 18rem;">
-<div class="card" style="width: 18rem;">
-  <img src=<?php echo $playlist['image']; ?> class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">  <?php echo $playlist['Playlist_Title']; ?></h5>
-    <p class="card-text">There are <?php echo $playlist['No_of_Songs']; ?> songs in this playlist. This is one of the best playlists created by ...</p>
-    <form method="post" action="songs-by-album.php">
-    <input type="hidden" name="pid" value="<?php echo $playlist['Playlist_Id'];?>"/>
-    <button type="submit" class="btn btn-primary">Songs</button>
-  </form>
-  </div>
+  <?php
+  $count = 0;
+  while ($playlist = $playlists->fetch_assoc()) {
+    // Start a new row after every third playlist
+    if ($count % 3 == 0 && $count != 0) {
+      echo '</div><div class="row">';
+    }
+  ?>
+    <div class="col-sm-4"> <!-- Adjusted to col-sm-4 for three columns -->
+      <div class="card" style="width: 18rem;">
+        <img src="<?php echo $playlist['image']; ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo $playlist['Playlist_Title']; ?></h5>
+          <p class="card-text">There are <?php echo $playlist['No_of_Songs']; ?> songs in this playlist. This is one of the best playlists created by ...</p>
+          <form method="post" action="songs-by-album.php">
+
+<input type="hidden" name="pid" value="<?php echo $playlist['Playlist_Id']; ?>"/>
+            <button type="submit" class="btn btn-primary">Songs</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  <?php
+    $count++;
+  }
+  ?>
 </div>
-    </div>
- <?php  
- }
- ?>   
-    </div>
-
-
-   
