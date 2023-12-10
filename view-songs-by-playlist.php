32d4822cb1b465fd09d2e53b68a6c5df92a77cb9
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-12-10 14:54:10 -0600

    Update view-songs-by-playlist.php

--- view-songs-by-playlist.php
+++ view-songs-by-playlist.php
@@ -5,7 +5,6 @@
     <thead>
       <tr>
       <th>Title</th>
-      <th>Stage Name</th>
       <th>Playlist</th>
       <th>Created On</th>
         <th>Updated At</th>
@@ -17,7 +16,6 @@
  ?>
    <tr>
      <td><?php echo $song['Song_Title']; ?></td>
-     <td><?php echo $song['Stage_Name']; ?></td>
      <td><?php echo $song['Playlist_Title']; ?></td>
      <td><?php echo $song['Created_At']; ?></td>
      <td><?php echo $song['Updated_At']; ?></td>
